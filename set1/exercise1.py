3279dfb0fc62e6a59f88100bf5c7d3e9afca2ff6
Sidney Mendez <sidneyreubenmendez@gmail.com>
2024-05-29 18:55:00 +1000

    First commit!!

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -4,7 +4,7 @@ TODO: write a python script that prints "Hello world!"
 
 Look on the course website for how to do this, and for how to run the tests.
 
-https://design-computing.github.io/md/week1
+
 
 """
 # 👇👇 Write your code here 👇👇
